049a786d0c169367522f7c0550f8f67c2d82499b
HIMANSHU YADAV <63348394+himanshuy946@users.noreply.github.com>
2023-06-05 23:22:14 +0530

    Create 030DeleteNodeLL.cpp

---
+++ 030DeleteNodeLL.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+/****************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    template <typename T>
+    class LinkedListNode
+    {
+    public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+
+void deleteNode(LinkedListNode<int> * node) {
+    if(node==NULL) return;
+    if(node->next==NULL){
+        node=NULL;
+        return;
+    }
+    node->data = node->next->data;
+    LinkedListNode<int> *temp = node->next;
+    node->next = node->next->next;
+    delete temp;
+}
